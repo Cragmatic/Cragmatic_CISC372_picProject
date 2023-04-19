4b4b7a2019d0d69b5afd00a8d46122ddd01de607
Craig Barber <RacgiMan@icloud.com>
2023-04-19 10:56:08 -0400

    pthreads implementation done. Not sure about timing

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
 
 image:image.c image.h
 	gcc -g image.c -o image -lm
+thread_image:thread_image.c thread_image.h
+	gcc -g thread_image.c -o thread_image -lm -lpthread
 clean:
-	rm -f image output.png
+	rm -f image thread_image output.png
---
+++ thread_image.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <string.h>
+#include "thread_image.h"
+#include "pthread.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int thread_count = 128;
+long long block_size;
+long leftover;
+ConvoluteThreadArgs sharedArgs;
+
+
+//An array of kernel matrices to be used for image convolution.  
+//The indexes of these match the enumeration from the header file. ie. algorithms[BLUR] returns the kernel corresponding to a box blur.
+Matrix algorithms[]={
+    {{0,-1,0},{-1,4,-1},{0,-1,0}},
+    {{0,-1,0},{-1,5,-1},{0,-1,0}},
+    {{1/9.0,1/9.0,1/9.0},{1/9.0,1/9.0,1/9.0},{1/9.0,1/9.0,1/9.0}},
+    {{1.0/16,1.0/8,1.0/16},{1.0/8,1.0/4,1.0/8},{1.0/16,1.0/8,1.0/16}},
+    {{-2,-1,0},{-1,1,1},{0,1,2}},
+    {{0,0,0},{0,1,0},{0,0,0}}
+};
+
+
+//getPixelValue - Computes the value of a specific pixel on a specific channel using the selected convolution kernel
+//Paramters: srcImage:  An Image struct populated with the image being convoluted
+//           x: The x coordinate of the pixel
+//          y: The y coordinate of the pixel
+//          bit: The color channel being manipulated
+//          algorithm: The 3x3 kernel matrix to use for the convolution
+//Returns: The new value for this x,y pixel and bit channel
+uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
+    int px,mx,py,my,i,span;
+    span=srcImage->width*srcImage->bpp;
+    // for the edge pixes, just reuse the edge pixel
+    px=x+1; py=y+1; mx=x-1; my=y-1;
+    if (mx<0) mx=0;
+    if (my<0) my=0;
+    if (px>=srcImage->width) px=srcImage->width-1;
+    if (py>=srcImage->height) py=srcImage->height-1;
+    uint8_t result=
+        algorithm[0][0]*srcImage->data[Index(mx,my,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[0][1]*srcImage->data[Index(x,my,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[0][2]*srcImage->data[Index(px,my,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[1][0]*srcImage->data[Index(mx,y,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[1][1]*srcImage->data[Index(x,y,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[1][2]*srcImage->data[Index(px,y,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[2][0]*srcImage->data[Index(mx,py,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[2][1]*srcImage->data[Index(x,py,srcImage->width,bit,srcImage->bpp)]+
+        algorithm[2][2]*srcImage->data[Index(px,py,srcImage->width,bit,srcImage->bpp)];
+    return result;
+}
+
+//convolute:  Applies a kernel matrix to an image
+//Parameters: srcImage: The image being convoluted
+//            destImage: A pointer to a  pre-allocated (including space for the pixel array) structure to receive the convoluted image.  It should be the same size as srcImage
+//            algorithm: The kernel matrix to use for the convolution
+//Returns: Nothing
+void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
+    int row,pix,bit,span;
+    span=srcImage->bpp*srcImage->bpp;
+    for (row=0;row<srcImage->height;row++){
+        for (pix=0;pix<srcImage->width;pix++){
+            for (bit=0;bit<srcImage->bpp;bit++){
+                destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)]=getPixelValue(srcImage,pix,row,bit,algorithm);
+            }
+        }
+    }
+}
+
+void* thread_convolute(void* rank) {
+    long my_rank = (long) rank;
+    long img_rows = sharedArgs.srcImage->height;
+    long img_cols = sharedArgs.srcImage->width;
+    long img_bits = sharedArgs.srcImage->bpp;
+
+    long starting_index = my_rank * block_size;
+
+    int starting_bit = starting_index % img_bits;
+    int leftover_pix = ((starting_index - starting_bit) / img_bits);
+    int starting_pix = leftover_pix % img_cols;
+    int starting_row = (leftover_pix - starting_pix) / img_cols;
+
+    long local_index = 0;
+
+    int cur_bit = starting_bit;
+    int cur_pix = starting_pix;
+    int cur_row = starting_row;
+
+    long long my_total_bits = block_size;
+    if (my_rank < leftover) {
+        my_total_bits++;
+    }
+
+    while(my_total_bits > 0) {
+        if (cur_bit == img_bits) {
+            cur_bit = 0;
+            cur_pix++;
+            if (cur_pix == img_cols) {
+                cur_pix = 0;
+                cur_row++;
+                if (cur_row == img_rows) {
+                    printf("This shouldn't happen; You did something wrong\n");
+                    return -1;
+                }
+            }
+        }
+        sharedArgs.destImage->data[local_index + starting_index]=getPixelValue(sharedArgs.srcImage, cur_pix, cur_row, cur_bit, algorithms[sharedArgs.type]);
+        local_index++;
+        cur_bit++;
+        my_total_bits--;
+    }
+    return 0;
+}
+
+//Usage: Prints usage information for the program
+//Returns: -1
+int Usage(){
+    printf("Usage: image <filename> <type>\n\twhere type is one of (edge,sharpen,blur,gauss,emboss,identity)\n");
+    return -1;
+}
+
+//GetKernelType: Converts the string name of a convolution into a value from the KernelTypes enumeration
+//Parameters: type: A string representation of the type
+//Returns: an appropriate entry from the KernelTypes enumeration, defaults to IDENTITY, which does nothing but copy the image.
+enum KernelTypes GetKernelType(char* type){
+    if (!strcmp(type,"edge")) return EDGE;
+    else if (!strcmp(type,"sharpen")) return SHARPEN;
+    else if (!strcmp(type,"blur")) return BLUR;
+    else if (!strcmp(type,"gauss")) return GAUSE_BLUR;
+    else if (!strcmp(type,"emboss")) return EMBOSS;
+    else return IDENTITY;
+}
+
+//main:
+//argv is expected to take 2 arguments.  First is the source file name (can be jpg, png, bmp, tga).  Second is the lower case name of the algorithm.
+int main(int argc,char** argv){
+
+    stbi_set_flip_vertically_on_load(0); 
+    if (argc!=3) return Usage();
+    char* fileName=argv[1];
+    if (!strcmp(argv[1],"pic4.jpg")&&!strcmp(argv[2],"gauss")){
+        printf("You have applied a gaussian filter to Gauss which has caused a tear in the time-space continum.\n");
+    }
+    enum KernelTypes type=GetKernelType(argv[2]);
+
+    Image srcImage,destImage,bwImage;   
+    srcImage.data=stbi_load(fileName,&srcImage.width,&srcImage.height,&srcImage.bpp,0);
+    if (!srcImage.data){
+        printf("Error loading file %s.\n",fileName);
+        return -1;
+    }
+    destImage.bpp=srcImage.bpp;
+    destImage.height=srcImage.height;
+    destImage.width=srcImage.width;
+    destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
+
+    long t1,t2;
+    t1=time(NULL);
+    clock_t start, end;
+    start = clock();
+ 
+    //convolute(&srcImage,&destImage,algorithms[type]);
+    sharedArgs.srcImage = &srcImage;
+    sharedArgs.destImage = &destImage;
+    sharedArgs.type = type;
+    block_size = (sharedArgs.srcImage->height * sharedArgs.srcImage->width * sharedArgs.srcImage->bpp / thread_count);
+    leftover = (sharedArgs.srcImage->height * sharedArgs.srcImage->width * sharedArgs.srcImage->bpp % thread_count);
+
+    pthread_t* thread_handles;
+    //thread_count = strtoi(argv[1],NULL,10);
+    thread_handles = (pthread_t*)malloc(thread_count*sizeof(pthread_t));
+    for (long thread=0; thread<thread_count; thread++) {
+        pthread_create(&thread_handles[thread], NULL, &thread_convolute, (void*) thread);
+    }
+    for (long thread=0; thread<thread_count; thread++) {
+        pthread_join(thread_handles[thread], NULL);
+    }
+    t2=time(NULL);
+    end = clock();
+    printf("Took %ld seconds\n",t2-t1);
+    double time_taken = (double) (end - start) / (double) CLOCKS_PER_SEC;
+    printf("Actually took %f seconds \n", time_taken);
+    stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
+    stbi_image_free(srcImage.data);
+    
+    free(destImage.data);
+   return 0;
+}
