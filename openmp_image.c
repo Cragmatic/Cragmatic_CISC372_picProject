2cb956b3c50d797afe552b353106a54e47eb152b
Craig Barber <RacgiMan@icloud.com>
2023-04-19 19:38:45 -0400

    added print statement dont forget to remove it

--- openmp_image.c
+++ openmp_image.c
@@ -60,6 +60,7 @@ void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
     #pragma omp parallel for collapse(3)
+    printf("threadId = %d \n", omp_get_thread_num());
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
